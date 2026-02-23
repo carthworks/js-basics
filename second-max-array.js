06a86ba9fbf235083b9ca2391ef74c32f45de452
karthikeyant <tkarthikeyan@pitowings.com>
2026-02-23 10:12:51 +0530

    second-MAX-noinarray

---
+++ second-max-array.js
@@ -0,0 +1,27 @@
+function findSecondLargest(arr) {
+    if (arr.length < 2) {
+        return "Array must have at least two elements";
+    }
+
+    let max = -Infinity;
+    let secondMax = -Infinity;
+
+    for (let n of arr) {
+        if (n > max) {
+            secondMax = max;
+            max = n;
+        } 
+        else if (n < max && n > secondMax) {
+            secondMax = n;
+        }
+    }
+
+    if (secondMax === -Infinity) {
+        return "No second largest (all elements equal)";
+    }
+
+    return { max, secondMax };
+}
+
+var arr = [3,2,7,9,11,45,12];
+console.log(findSecondLargest(arr));
